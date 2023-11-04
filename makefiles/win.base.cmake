33b689d03ac111f54d0c5ea5c0a9dea1e7f33c85
Ciallo <dison2233@126.com>
2023-11-05 04:35:16 +0800

    路径

--- makefiles/win.base.cmake
+++ makefiles/win.base.cmake
@@ -12,8 +12,8 @@ SET(
         ${SOURCESDK_LIB}/public/win64/interfaces.lib
         ${SOURCESDK_LIB}/public/win64/mathlib.lib
         spdlog
-        libraries/dyncall/build/dyncall/Release/dynload_s
-        libraries/dyncall/build/dynload/Release/dyncall_s
+        ../libraries/dyncall/build/dyncall/Release/dynload_s
+        ../libraries/dyncall/build/dynload/Release/dyncall_s
         distorm
         funchook-static
 )
