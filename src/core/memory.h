e9c8f13dfa7666f09c004a5b605d35e54a802bf0
Ciallo <dison2233@126.com>
2023-11-05 16:26:51 +0800

    测试#2

--- src/core/memory.h
+++ src/core/memory.h
@@ -4,13 +4,15 @@
 #ifdef _WIN32
     #define ROOTBIN "/bin/win64/"
     #define GAMEBIN "/csgo/bin/win64/"
+    #define MODULE_PREFIX ""
+    #define MODULE_EXT ".dll"
 #else
     #define ROOTBIN "/bin/linuxsteamrt64/"
     #define GAMEBIN "/csgo/bin/linuxsteamrt64/"
+    #define MODULE_PREFIX "lib"
+    #define MODULE_EXT ".so"
 #endif
 
-#define MODULE_PREFIX "lib"
-#define MODULE_EXT ".so"
 
 int GetModuleInformation(void *hModule, void **base, size_t *length);
 void* FindSignature(const char* moduleName, const char* bytesStr);
