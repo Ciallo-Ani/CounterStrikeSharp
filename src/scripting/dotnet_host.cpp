c21ee258f8c9e3501f2741f10d4f24e8b8af3b09
Ciallo <dison2233@126.com>
2023-11-06 22:51:40 +0800

    w_char兼容

--- src/scripting/dotnet_host.cpp
+++ src/scripting/dotnet_host.cpp
@@ -20,6 +20,8 @@
 
 #include <cassert>
 #include <iostream>
+#include <sstream>
+#include <metamod_oslink.h>
 
 #include "core/log.h"
 #include "core/utils.h"
@@ -37,51 +39,33 @@ load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t 
 }  // namespace
 
 namespace {
-// Forward declarations
-void *load_library(const char_t *);
-void *get_export(void *, const char *);
-
-#ifdef _WINDOWS
-void *load_library(const char_t *path) {
-    HMODULE h = ::LoadLibraryW(path);
-    assert(h != nullptr);
-    return (void *)h;
-}
-
-void *get_export(void *h, const char *name) {
-    void *f = ::GetProcAddress((HMODULE)h, name);
-    assert(f != nullptr);
-    return f;
-}
-#else
-void *load_library(const char_t *path) {
-    void *h = dlopen(path, RTLD_LAZY | RTLD_LOCAL);
-    assert(h != nullptr);
-    return h;
-}
-void *get_export(void *h, const char *name) {
-    void *f = dlsym(h, name);
-    assert(f != nullptr);
-    return f;
-}
-#endif
 
 // <SnippetLoadHostFxr>
 // Using the nethost library, discover the location of hostfxr and get exports
 bool load_hostfxr() {
     std::string baseDir = counterstrikesharp::utils::PluginDirectory();
 
+#ifdef _WIN32
+    std::string buffer = std::string(baseDir + "/dotnet/host/fxr/7.0.11/hostfxr.dll");
+#else
     std::string buffer = std::string(baseDir + "/dotnet/host/fxr/7.0.11/libhostfxr.so");
+#endif
 
-    CSSHARP_CORE_TRACE("Loading hostfxr from {0}", buffer.c_str());
+    CSSHARP_CORE_INFO("Loading hostfxr from {0}", buffer.c_str());
 
     // Load hostfxr and get desired exports
-    void *lib = load_library((char_t *)buffer.c_str());
-    init_fptr = (hostfxr_initialize_for_runtime_config_fn)get_export(
-        lib, "hostfxr_initialize_for_runtime_config");
-    get_delegate_fptr =
-        (hostfxr_get_runtime_delegate_fn)get_export(lib, "hostfxr_get_runtime_delegate");
-    close_fptr = (hostfxr_close_fn)get_export(lib, "hostfxr_close");
+    auto lib = dlmount(buffer.c_str());
+
+    CSSHARP_CORE_INFO("lib -> {}", (void*)lib);
+
+    if (lib == nullptr) {
+        CSSHARP_CORE_ERROR("Failed to load {}", buffer);
+        return false;
+    }
+    
+    init_fptr = (hostfxr_initialize_for_runtime_config_fn)dlsym(lib, "hostfxr_initialize_for_runtime_config");
+    get_delegate_fptr = (hostfxr_get_runtime_delegate_fn)dlsym(lib, "hostfxr_get_runtime_delegate");
+    close_fptr = (hostfxr_close_fn)dlsym(lib, "hostfxr_close");
 
     return (init_fptr && get_delegate_fptr && close_fptr);
 }
@@ -91,19 +75,24 @@ bool load_hostfxr() {
 // Load and initialize .NET Core and get desired function pointer for scenario
 load_assembly_and_get_function_pointer_fn get_dotnet_load_assembly(const char_t *config_path) {
     // Load .NET Core
-    void *load_assembly_and_get_function_pointer = nullptr;
     int rc = init_fptr(config_path, nullptr, &cxt);
     if (rc != 0 || cxt == nullptr) {
-        std::cerr << "Init failed: " << std::hex << std::showbase << rc << std::endl;
+        std::ostringstream oss;
+        oss << std::hex << std::showbase;
+        CSSHARP_CORE_ERROR("Init failed: {}. Return Code: {}", oss.str(), rc);
         close_fptr(cxt);
         return nullptr;
     }
 
     // Get the load assembly function pointer
-    rc = get_delegate_fptr(cxt, hdt_load_assembly_and_get_function_pointer,
-                           &load_assembly_and_get_function_pointer);
-    if (rc != 0 || load_assembly_and_get_function_pointer == nullptr)
-        std::cerr << "Get delegate failed: " << std::hex << std::showbase << rc << std::endl;
+    void* load_assembly_and_get_function_pointer = nullptr;
+    rc = get_delegate_fptr(cxt, hdt_load_assembly_and_get_function_pointer, &load_assembly_and_get_function_pointer);
+    if (rc != 0 || load_assembly_and_get_function_pointer == nullptr) {
+        std::ostringstream oss;
+        oss << std::hex << std::showbase;
+        CSSHARP_CORE_ERROR("Get delegate failed: {}. Return Code: {}", oss.str(), rc);
+        return nullptr;
+    }
 
     // close_fptr(cxt);
     return (load_assembly_and_get_function_pointer_fn)load_assembly_and_get_function_pointer;
@@ -125,30 +114,42 @@ bool CDotNetManager::Initialize() {
 
     CSSHARP_CORE_INFO(".NET Runtime Initialised.");
 
-    std::string wideStr =
-        std::string((baseDir + "/api/CounterStrikeSharp.API.runtimeconfig.json").c_str());
+    std::string wideStr = std::string(baseDir + "/api/CounterStrikeSharp.API.runtimeconfig.json");
+
+    CSSHARP_CORE_INFO("wideStr -> {}", wideStr);
 
     load_assembly_and_get_function_pointer_fn load_assembly_and_get_function_pointer = nullptr;
-    load_assembly_and_get_function_pointer = get_dotnet_load_assembly((char_t*)wideStr.c_str());
-    assert(load_assembly_and_get_function_pointer != nullptr &&
-           "Failure: get_dotnet_load_assembly()");
+    load_assembly_and_get_function_pointer = get_dotnet_load_assembly(converter.from_bytes(wideStr).c_str());
+    if (load_assembly_and_get_function_pointer == nullptr) {
+        CSSHARP_CORE_ERROR("Failure: get_dotnet_load_assembly()");
+        return false;
+    }
 
-    const std::string dotnetlib_path =
-        std::string((baseDir + "/api/CounterStrikeSharp.API.dll").c_str());
-    const char_t *dotnet_type = (char_t*)"CounterStrikeSharp.API.Core.Helpers, CounterStrikeSharp.API";
+    auto dotnetlib_path = converter.from_bytes(std::string(baseDir + "/api/CounterStrikeSharp.API.dll"));
+    auto dotnet_type = converter.from_bytes("CounterStrikeSharp.API.Core.Helpers, CounterStrikeSharp.API");
+
+    CSSHARP_CORE_INFO("dotnetlib_path -> {}", converter.to_bytes(dotnetlib_path));
+    CSSHARP_CORE_INFO("dotnet_type -> {}", converter.to_bytes(dotnet_type));
     // Namespace, assembly name
 
+    // FIXME
     typedef int(CORECLR_DELEGATE_CALLTYPE * custom_entry_point_fn)();
     custom_entry_point_fn entry_point = nullptr;
-    int rc = load_assembly_and_get_function_pointer((char_t*)dotnetlib_path.c_str(), dotnet_type,
-                                                    (char_t *)"LoadAllPlugins", UNMANAGEDCALLERSONLY_METHOD,
-                                                    nullptr, (void **)&entry_point);
+
+    int rc = load_assembly_and_get_function_pointer(
+        dotnetlib_path.c_str(), 
+        dotnet_type.c_str(), 
+        converter.from_bytes("LoadAllPlugins").c_str(),
+        UNMANAGEDCALLERSONLY_METHOD,
+        nullptr, 
+        (void **)&entry_point);
+
     assert(rc == 0 && entry_point != nullptr &&
            "Failure: load_assembly_and_get_function_pointer()");
 
     const bool success = entry_point();
     if (!success) {
-        CSSHARP_CORE_ERROR("Failed to initialize .NET");
+        CSSHARP_CORE_ERROR("not success! Failed to initialize .NET");
         return false;
     }
 
