d3c5977e7b6bf54c5175fa94b29b379e90ec33c8
Ciallo <dison2233@126.com>
2023-11-07 22:45:47 +0800

    dynamic binary path

--- managed/CounterStrikeSharp.API/Core/Constants.cs
+++ managed/CounterStrikeSharp.API/Core/Constants.cs
@@ -1,7 +1,23 @@
+using System.Runtime.InteropServices;
+
 namespace CounterStrikeSharp.API.Core;
 
 public static class Constants
 {
-    public const string ROOT_BINARY_PATH = "/bin/linuxsteamrt64/";
-    public const string GAME_BINARY_PATH = "/csgo/bin/linuxsteamrt64/";
+    public static string ROOT_BINARY_PATH { get; }
+    public static string GAME_BINARY_PATH { get; }
+
+    static Constants()
+    {
+        if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+        {
+            ROOT_BINARY_PATH = "/bin/linuxsteamrt64/";
+            GAME_BINARY_PATH = "/csgo/bin/linuxsteamrt64/";
+        }
+        else
+        {
+            ROOT_BINARY_PATH = "/bin/win64/";
+            GAME_BINARY_PATH = "/csgo/bin/win64/";
+        }
+    }
 }
