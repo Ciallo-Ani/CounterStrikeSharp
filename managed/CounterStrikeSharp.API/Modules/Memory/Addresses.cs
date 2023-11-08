be60ef8907b835994702c4f5510bed1cc6c6e6e8
Ciallo <dison2233@126.com>
2023-11-09 02:25:35 +0800

    fix binary path incorrect

--- managed/CounterStrikeSharp.API/Modules/Memory/Addresses.cs
+++ managed/CounterStrikeSharp.API/Modules/Memory/Addresses.cs
@@ -6,13 +6,13 @@ namespace CounterStrikeSharp.API.Modules.Memory;
 public class Addresses
 {
     public static string EnginePath = 
-        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX, "engine2", Constants.MODULE_EXT);
+        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX + "engine2" + Constants.MODULE_EXT);
     public static string Tier0Path = 
-        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX, "tier0", Constants.MODULE_EXT);
+        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX + "tier0" + Constants.MODULE_EXT);
     public static string ServerPath = 
-        Path.Join(Server.GameDirectory, Constants.GAME_BINARY_PATH, Constants.MODULE_PREFIX, "server", Constants.MODULE_EXT);
+        Path.Join(Server.GameDirectory, Constants.GAME_BINARY_PATH, Constants.MODULE_PREFIX + "server" + Constants.MODULE_EXT);
     public static string SchemaSystemPath =
-        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX, "schemasystem", Constants.MODULE_EXT);
+        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX + "schemasystem" + Constants.MODULE_EXT);
     public static string VScriptPath = 
-        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX, "vscript", Constants.MODULE_EXT);
+        Path.Join(Server.GameDirectory, Constants.ROOT_BINARY_PATH, Constants.MODULE_PREFIX + "vscript" + Constants.MODULE_EXT);
 }
